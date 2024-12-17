08ddda042e35d02c9cc0f84526b684f89f16e049
agentelement <agentelement@protonmail.com>
2024-12-17 14:52:31 -0700

    proto: top_down_search

--- src/assembly.rs
+++ src/assembly.rs
@@ -1,6 +1,14 @@
 use crate::molecule::Molecule;
 
+fn top_down_search(m: &Molecule) {
+    for (left, right) in m.partitions().unwrap() {
+        println!("left: {:?}", left);
+        println!("right: {:?}", right);
+    }
+}
+
 // Compute the assembly index of a molecule
 pub fn index(m: &Molecule) -> u32 {
+    top_down_search(m);
     0
 }
