17bc28b6dd470177ea22094313163af744ce611e
agentelement <agentelement@protonmail.com>
2025-02-03 14:27:58 -0700

    build: rename binary to `orca`

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -1,13 +1,15 @@
 use std::path::PathBuf;
 
-use fastassembly::assembly::index;
+use orca::assembly::index;
 
 use criterion::{criterion_group, criterion_main, Criterion};
-use fastassembly::loader;
+use orca::loader;
 
 pub fn criterion_benchmark(c: &mut Criterion) {
-    let molecule = loader::parse(&PathBuf::from("data/aspartic.sdf")).unwrap();
-    c.bench_function("aspartic", |b| b.iter(|| index(&molecule)));
+    for str in ["aspartic", ""] {
+        let molecule = loader::parse(&PathBuf::from(format!("data/{str}.sdf"))).unwrap();
+        c.bench_function("aspartic", |b| b.iter(|| index(&molecule)));
+    }
 }
 
 criterion_group!(benches, criterion_benchmark);
