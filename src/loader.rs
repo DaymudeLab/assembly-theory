f55ae668fe1bf3aa477d3bf60372307e44b0b53d
agentelement <agentelement@protonmail.com>
2025-03-06 17:13:49 -0700

    doc: remove whitespace and reflow lines in loader

--- src/loader.rs
+++ src/loader.rs
@@ -1,5 +1,5 @@
-//! A loader to parse molecules data files.
-//! 
+//! Parse molecule files in the `.mol` file format
+//!
 //! # Example
 //! ```
 //! # use std::fs;
@@ -9,7 +9,7 @@
 //! # let path = PathBuf::from(format!("./data/checks/benzene.mol"));
 //! // Read a molecule data file as a string of lines
 //! let molfile = fs::read_to_string(path).expect("Cannot read input file.");
-//! 
+//!
 //! let molecule = loader::parse_molfile_str(&molfile).expect("Cannot parse molfile.");
 //! # Ok(())
 //! # }
@@ -20,8 +20,9 @@ use std::error::Error;
 use std::fmt::Display;
 
 /// Molecule data file parsing functions return a `ParserError` type when an error occurs.
-/// 
-/// Describe the specifc error type along with the line number of the molecule data file where error occured. 
+///
+/// Describe the specifc error type along with the line number of the molecule data file where
+/// error occured.
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum ParserError {
     /// Atom count is not an integer value, occurs while parsing the counts line.
@@ -30,13 +31,15 @@ pub enum ParserError {
     BondCountNotInt(usize),
     /// The version of the molecule data file is not `V2000`.
     FileVersionIsNotV2000(usize),
-    /// Cannot parse the atom's symbol as one of the exisiting [`crate::molecule::Atom`] symbols, occurs while parsing the atom line.
+    /// Cannot parse the atom's symbol as one of the exisiting [`crate::molecule::Atom`] symbols,
+    /// occurs while parsing the atom line.
     BadElementSymbol(usize, String),
     /// Cannot parse the Bond Number as an integer value, occurs while parsing the bond line.
     BadBondNumber(usize),
     /// Cannot parse the Bond Type as an integer value, occurs while parsing the bond line.
     BondTypeNotInt(usize),
-    /// Cannot parse the Bond Type as one of the exisiting [`crate::molecule::Bond`] types, occurs while parsing the bond line.
+    /// Cannot parse the Bond Type as one of the exisiting [`crate::molecule::Bond`] types, occurs
+    /// while parsing the bond line.
     BondTypeOutOfBounds(usize),
     /// Unknown error which if occured, should be reported to the maintainers of the crate.
     ThisShouldNotHappen,
@@ -46,15 +49,17 @@ pub enum ParserError {
 
 impl Error for ParserError {}
 
-/// Parse a `.sdf` molecule data file and return a [`crate::molecule::Molecule`] object. `To be implemented`
+/// Parse a `.sdf` molecule data file and return a [`crate::molecule::Molecule`] object. `To be
+/// implemented`
 pub fn parse_sdfile_str(_input: &str) -> Result<Molecule, ParserError> {
     todo!("SDfile parser unimplemented!")
 }
 
-/// Parse a `.mol` molecule data file and return a [`crate::molecule::Molecule`] object.
-/// 
-/// Input the Mol file as a string of lines. Incase of an error, a [`self::ParserError`] is thrown.
-/// 
+/// Parse a string containing the contents of a `.mol` molecule data file and return a
+/// [`crate::molecule::Molecule`] object.
+///
+/// If the file string is malformed, a [`self::ParserError`] is thrown.
+///
 /// # Example
 /// ```
 /// # use std::fs;
@@ -64,7 +69,7 @@ pub fn parse_sdfile_str(_input: &str) -> Result<Molecule, ParserError> {
 /// # let path = PathBuf::from(format!("./data/checks/benzene.mol"));
 /// // Read a molecule data file as a string of lines
 /// let molfile = fs::read_to_string(path).expect("Cannot read input file.");
-/// 
+///
 /// let molecule = loader::parse_molfile_str(&molfile).expect("Cannot parse molfile.");
 /// # Ok(())
 /// # }
