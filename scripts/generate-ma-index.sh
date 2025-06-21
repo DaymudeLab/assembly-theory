f0d04e3835fb1ffbbc8456deba5d2461ac378467
Josh Daymude <jdaymude@asu.edu>
2025-06-20 22:46:07 -0700

    Add assembly_cpp (Seet et al., 2024) as ground truth, fix coconut_55

--- scripts/generate-ma-index.sh
+++ scripts/generate-ma-index.sh
@@ -16,7 +16,7 @@ done
 
 # Let the user choose which executable should generate ground truth.
 PS3="Calculate assembly indices using: "
-select exec_choice in "assembly_go" "assembly-theory"
+select exec_choice in "assembly_go (Jirasek et al., 2024)" "assembly_cpp (Seet et al., 2024)" "assembly-theory"
 do
     case $REPLY in
         1)
@@ -29,6 +29,15 @@ do
             break
             ;;
         2)
+            if [ ! -f "assembly_cpp" ]; then
+                echo -n "ERROR: Missing ./assembly_cpp executable "
+                echo "(provided privately by Seet et al.)."
+                exit 1
+            fi
+            executable="./assembly_cpp"
+            break
+            ;;
+        3)
             echo "Building a release version of assembly-theory..."
             cargo build --release
             executable="./../target/release/assembly-theory"
@@ -50,7 +59,19 @@ for direntry in "$dataset"/*.mol
 do
     molfile=$(basename "$direntry")
     echo -ne "\r\e[K$exec_choice: Calculating assembly index of $molfile..."
-    maindex=$("$executable" "$direntry")
+
+    # assembly_go and assembly-theory expect "<molecule>.mol" but assembly_cpp
+    # expects only "<molecule>" with the ".mol" part stripped off. Also,
+    # assembly_cpp prints a ton of unnecessary information requiring some
+    # parsing to get just the assembly index. Lastly, assembly_cpp generates
+    # auxiliary output files that need to be removed.
+    if [ $executable = "./assembly_cpp" ]; then
+        molpath_stripped=$(echo "$direntry" | sed -e "s/.mol//g")
+        maindex=$("$executable" "$molpath_stripped" -pathway=0 | tail -n 1 | awk '{print $NF}')
+        rm "${molpath_stripped}Out"
+    else
+        maindex=$("$executable" "$direntry")
+    fi
     echo "$molfile,$maindex" >> "$mafile"
 done
 echo ""
