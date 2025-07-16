014debdd17b83ce3b14087e580b82fbb3bf4afd4
Josh Daymude <jdaymude@asu.edu>
2025-07-16 09:10:22 -0700

    proto: Add all CLI options, but leave interface unchanged for now

--- src/main.rs
+++ src/main.rs
@@ -3,113 +3,243 @@ use std::path::PathBuf;
 
 use anyhow::{bail, Context, Result};
 use assembly_theory::assembly::{index_search, serial_index_search, Bound};
-use assembly_theory::{loader, molecule::Molecule};
+use assembly_theory::loader;
 use clap::{Args, Parser, ValueEnum};
-
-#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum, Debug)]
-enum Bounds {
-    Log,
-    IntChain,
-    VecChain,
-}
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Cli {
-    path: PathBuf,
+    /// Path to .mol file to compute the assembly index for.
+    molpath: PathBuf,
 
-    #[arg(short, long)]
-    /// Print out search space, duplicate subgraphs, and assembly index
+    /// Print molecule graph information, skipping assembly index calculation.
+    #[arg(long)]
+    molinfo: bool,
+
+    /// Print the assembly index, number of non-overlapping isomorphic subgraph
+    /// pairs, and number of fragment structures searched (i.e., the "search
+    /// space"). Note that the search space may be nondeterministic when run
+    /// using parallelism.
+    #[arg(long)]
     verbose: bool,
 
+    /// Strategy for enumerating connected, non-induced subgraphs.
+    #[arg(long, value_enum, default_value_t = EnumerateMode::GrowErode)]
+    enumerate: EnumerateMode,
+
+    /// Algorithm for graph canonization.
+    #[arg(long, value_enum, default_value_t = CanonizeMode::Nauty)]
+    canonize: CanonizeMode,
+
+    /// Parallelization strategy for the search phase.
+    #[arg(long, value_enum, default_value_t = ParallelMode::Always)]
+    parallel: ParallelMode,
+
+    /// Use dynamic programming memoization in the search phase.
+    #[arg(long)]
+    memoize: bool,
+
+    /// Bounding strategies to apply in the search phase.
     #[command(flatten)]
-    boundgroup: Option<BoundGroup>,
+    boundsgroup: Option<BoundsGroup>,
 
-    #[arg(long)]
-    /// Dump out molecule graph
-    molecule_info: bool,
+    /// Strategy for performing graph kernelization during the search phase.
+    #[arg(long, value_enum, default_value_t = KernelMode::None)]
+    kernel: KernelMode,
+}
 
-    #[arg(long)]
-    /// Disable all parallelism
-    serial: bool,
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum, Debug)]
+enum EnumerateMode {
+    /// Grow connected subgraphs from each edge using BFS.
+    Bfs,
+    /// Like Bfs, but at each level of the BFS, prune any subgraphs that do not
+    /// have isomorphic components since these will not be useful later.
+    BfsPrune,
+    /// From a subgraph, choose an edge from its frontier and recursively grow
+    /// the subgraph by this edge or erode it by discarding the edge.
+    GrowErode,
+    /// An iterative (memory-efficient) implementation of GrowErode.
+    GrowErodeIterative,
+}
+
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum, Debug)]
+enum CanonizeMode {
+    /// Use the Nauty algorithm of McKay & Piperno (2014).
+    Nauty,
+    /// Use the algorithm of Faulon et al. (2004).
+    Faulon,
+    /// Use a fast tree canonization algorithm if applicable; else use Nauty.
+    TreeNauty,
+    /// Use a fast tree canonization algorithm if applicable; else use Faulon.
+    TreeFaulon,
+}
+
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum, Debug)]
+enum ParallelMode {
+    /// No parallelism.
+    None,
+    /// Create a task pool form the recursion's first level only.
+    DepthOne,
+    /// Spawn a new thread at every recursive call.
+    Always,
 }
 
 #[derive(Args, Debug)]
 #[group(required = false, multiple = false)]
-struct BoundGroup {
+struct BoundsGroup {
+    /// Do not use any bounding strategy during the search phase.
     #[arg(long)]
-    /// Run branch-and-bound index search with no bounds
     no_bounds: bool,
 
+    /// Apply the specified bounding strategies during the search phase.
     #[arg(long, num_args = 1..)]
-    /// Run branch-and-bound index search with only specified bounds
-    bounds: Vec<Bounds>,
+    bounds: Vec<BoundOption>,
 }
 
-fn make_boundlist(u: &[Bounds]) -> Vec<Bound> {
-    let mut boundlist = u
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum, Debug)]
+enum BoundOption {
+    /// The trivial bound of log_2(b), where b is # of remaining bonds.
+    Log,
+    /// TODO
+    Int,
+    /// TODO
+    VecSimple,
+    /// TODO
+    VecSmallFrags,
+    /// TODO
+    CoverSort,
+    /// TODO
+    CoverNoSort,
+    /// TODO
+    CliqueBudget,
+}
+
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum, Debug)]
+enum KernelMode {
+    /// No kernelization.
+    None,
+    /// Only kernelize the original molecule.
+    Once,
+    /// Kernelize the original molecule and the recursion's first level only.
+    DepthOne,
+    /// Perform kernelization at every recursive step.
+    Always,
+}
+
+/// Convert CLI BoundOptions to a list of assembly::Bounds.
+fn make_boundlist(bounds: &[BoundOption]) -> Vec<Bound> {
+    let mut boundlist = bounds
         .iter()
         .flat_map(|b| match b {
-            Bounds::Log => vec![Bound::Log],
-            Bounds::IntChain => vec![Bound::IntChain],
-            Bounds::VecChain => vec![Bound::VecChainSimple, Bound::VecChainSmallFrags],
+            BoundOption::Log => vec![Bound::Log],
+            BoundOption::Int => vec![Bound::IntChain],
+            BoundOption::VecSimple => vec![Bound::VecChainSimple],
+            BoundOption::VecSmallFrags => vec![Bound::VecChainSmallFrags],
+            _ => {
+                println!("WARNING: Ignoring bound not implemented yet");
+                vec![]
+            },
         })
         .collect::<Vec<_>>();
     boundlist.dedup();
     boundlist
 }
 
-fn index_message(mol: &Molecule, bounds: &[Bound], verbose: bool, serial: bool) -> String {
-    let (index, duplicates, space) = if serial {
-        serial_index_search(mol, bounds)
-    } else {
-        index_search(mol, bounds)
-    };
-    if verbose {
-        let mut message = String::new();
-        message.push_str(&format!("Assembly index: {index}\n"));
-        message.push_str(&format!("Duplicate subgraph pairs: {duplicates}\n"));
-        message.push_str(&format!("Search space: {space}"));
-        message
-    } else {
-        index.to_string()
-    }
-}
-
 fn main() -> Result<()> {
+    // Parse command line arguments.
     let cli = Cli::parse();
-    let molfile = fs::read_to_string(&cli.path).context("Cannot read input file.")?;
-    let molecule = loader::parse_molfile_str(&molfile).context("Cannot parse molfile.")?;
+
+    // Load the .mol file as a molecule::Molecule.
+    let molfile = fs::read_to_string(&cli.molpath)
+        .context("Cannot read input file.")?;
+    let molecule = loader::parse_molfile_str(&molfile)
+        .context("Cannot parse molfile.")?;
     if molecule.is_malformed() {
-        bail!("Bad input! Molecule has self-loops or doubled edges")
+        bail!("Bad input! Molecule has self-loops or multi-edges.")
     }
 
-    if cli.molecule_info {
+    // If --molinfo is set, print molecule graph and exit.
+    if cli.molinfo {
         println!("{}", molecule.info());
         return Ok(());
     }
 
-    let output = match cli.boundgroup {
-        None => index_message(
-            &molecule,
-            &[
-                Bound::IntChain,
-                Bound::VecChainSimple,
-                Bound::VecChainSmallFrags,
-            ],
-            cli.verbose,
-            cli.serial,
-        ),
-        Some(BoundGroup {
-            no_bounds: true, ..
-        }) => index_message(&molecule, &[], cli.verbose, cli.serial),
-        Some(BoundGroup {
-            no_bounds: false,
-            bounds,
-        }) => index_message(&molecule, &make_boundlist(&bounds), cli.verbose, cli.serial),
+    // TODO: Do something about EnumerateModes.
+    match cli.enumerate {
+        EnumerateMode::Bfs => {
+            println!("WARNING: Ignoring EnumerateMode::Bfs; not implemented yet");
+        }
+        EnumerateMode::BfsPrune => {
+            println!("WARNING: Ignoring EnumerateMode::BfsPrune; not implemented yet");
+        }
+        EnumerateMode::GrowErode => {
+            println!("Using recursive grow-erode for subgraph enumeration");
+        }
+        EnumerateMode::GrowErodeIterative => {
+            println!("WARNING: Ignoring EnumerateMode::GrowErodeIterative; not implemented yet");
+        }
+    }
+
+    // TODO: Do something about CanonizeModes.
+    match cli.canonize {
+        CanonizeMode::Nauty => {
+            println!("Using nauty for graph canonization");
+        }
+        CanonizeMode::Faulon => {
+            println!("WARNING: Ignoring CanonizeMode::Faulon; not implemented yet");
+        }
+        CanonizeMode::TreeNauty => {
+            println!("WARNING: Ignoring CanonizeMode::TreeNauty; not implemented yet");
+        }
+        CanonizeMode::TreeFaulon => {
+            println!("WARNING: Ignoring CanonizeMode::TreeFaulon; not implemented yet");
+        }
+    }
+
+    // TODO: Do something about ParallelModes.
+    let parallel = match cli.parallel {
+        ParallelMode::None => false,
+        ParallelMode::DepthOne => {
+            println!("WARNING: Ignoring ParallelMode::DepthOne; not implemented yet");
+            false
+        },
+        ParallelMode::Always => true,
     };
 
-    println!("{output}");
+    // Handle bounding strategy CLI arguments.
+    let boundlist: &[Bound] = match cli.boundsgroup {
+        // By default, use a combination of the integer and vector bounds.
+        None => &[
+            Bound::IntChain,
+            Bound::VecChainSimple,
+            Bound::VecChainSmallFrags,
+        ],
+        // If --no-bounds is set, do not use any bounds.
+        Some(BoundsGroup {
+            no_bounds: true, ..
+        }) => &[],
+        // Otherwise, use the bounds that were specified.
+        Some(BoundsGroup {
+            no_bounds: false, bounds,
+        }) => &make_boundlist(&bounds),
+    };
+
+    // Call index calculation with all the various options.
+    // TODO: Rework with the full list of options.
+    let (index, dup_pairs, search_size) = if parallel {
+        index_search(&molecule, boundlist)
+    } else {
+        serial_index_search(&molecule, boundlist)
+    };
+
+    // Print final output, depending on --verbose.
+    if cli.verbose {
+        println!("Assembly Index: {index}");
+        println!("# Non-Overlapping Isomorphic Subgraph Pairs: {dup_pairs}");
+        println!("Search Space Size: {search_size}");
+    } else {
+        println!("{index}");
+    }
 
     Ok(())
 }
