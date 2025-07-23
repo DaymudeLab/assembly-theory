235ab31bcb66502053485e15ff4e7af60aff181c
Garrett-Pz <garrettparzych@gmail.com>
2025-07-23 12:21:27 -0700

    Add doc comments to kernels.rs

--- src/kernels.rs
+++ src/kernels.rs
@@ -1,19 +1,29 @@
 //! Kernelize match-compatibility graphs to improve top-down search efficiency.
 //!
-//! TODO: Longer explanation of what that means from @Garrett-Pz.
+//! The problem of computing the minimum assembly index of a molecule can be
+//! reduced to finding the maximum weight clique in the compatibility graph
+//! definined in ```reductions.rs```. Strucutral properties of this graph can
+//! be used to determine vertices (each corresponding to a duplicatable subgraph
+//! match) that will either never be used in an optimal solution or will be used
+//! in some optimal solution. We call the process of identifying these vertices 
+//! kernelization. Uses the strategies of neighborhood removal, isolated vertex 
+//! removal, and domination as described in section 5.2 of
+//! [Lamm et al. (2019)](https://doi.org/10.1137/1.9781611975499.12). (Note that
+//! they solve the equivalent problem of weighted independent set).
 
 use clap::ValueEnum;
 
 /// Graph kernelization strategy when searching using the clique reduction.
-// TODO: Need @Garrett-Pz to write better descriptions of these methods.
 #[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
 pub enum KernelMode {
-    /// No kernelization.
+    /// Do not apply any kernelizations.
     None,
-    /// Only kernelize the original molecule.
+    /// Apply kernels only after the initial construction of the compatibility
+    /// graph.
     Once,
-    /// Kernelize the original molecule and the recursion's first level only.
+    /// Apply kernels after the initial construction of the compability 
+    /// graph, and again after any fragmentations of the full molecule.
     DepthOne,
-    /// Perform kernelization at every recursive step.
+    /// Apply kernels after every fragmentation step.
     Always,
 }
