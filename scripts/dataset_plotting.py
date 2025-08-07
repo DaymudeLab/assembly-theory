749cefc15132f4a8d1b7486f841bff057a249b2e
Garrett-Pz <garrettparzych@gmail.com>
2025-08-07 16:47:07 -0700

    Fix output directory bug

--- scripts/dataset_plotting.py
+++ scripts/dataset_plotting.py
@@ -12,16 +12,20 @@ parser.add_argument("--name", help="Evaluation name")
 args = parser.parse_args()
 file_str = args.file
 if len(file_str) < 4 or file_str[-4:] != '.out':
-    raise Exception("Invaldi input type")
+    raise Exception("Invald input type")
 
 out_dir = args.out if args.out else os.path.dirname(file_str)
-data_name = args.name if args.name else os.path.basename(file_str)
+if out_dir[-1] != "/":
+    out_dir += "/"
+os.makedirs(out_dir, exist_ok=True)
+data_name = args.name if args.name else os.path.basename(file_str)[:-4]
 out_file = out_dir + data_name + '.svg'
 
 # Plotting
+# dataframe columns: File, Atoms, Bonds, Rings, Index, Dupes, StatesSearched
 df = pd.read_csv(args.file)
 
-plt.scatter(x=df['Dupes'], y=df['StatesSearched'])
+plt.scatter(x=df['Dupes'], y=df['StatesSearched'], c=df['Bonds'])
 plt.xlabel('Num duplicatable matches')
 plt.ylabel('States Searched')
 plt.savefig(out_file, format='svg')
