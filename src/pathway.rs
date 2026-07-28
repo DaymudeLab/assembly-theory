b09d68eefdcc605822bd8748a51c92a3667d4414
Josh Daymude <jdaymude@asu.edu>
2026-07-28 14:48:40 -0700

    feat: Rework the pathway DAG node/edge labels as fragment BitSets

--- src/pathway.rs
+++ src/pathway.rs
@@ -8,13 +8,17 @@ use petgraph::{
     graph::{DiGraph, EdgeIndex, NodeIndex},
 };
 
-use crate::{matches::Matches, molecule::Molecule, utils::subgraph_from_edge_mask};
+use crate::{
+    canonize::{canonize, CanonizeMode},
+    matches::Matches,
+    molecule::Molecule,
+};
 
 /// Assembly pathway of pairwise joining operations yielding the target graph.
 pub struct Pathway {
     /// A directed, acyclic graph representation of the assembly pathway; see
     /// [`Pathway::dag`] for details.
-    dag: DiGraph<String, ()>,
+    dag: DiGraph<BitSet, BitSet>,
 }
 
 impl Pathway {
@@ -57,7 +61,7 @@ impl Pathway {
 
         // Use the bag of pieces to construct all duplicate fragments across
         // matches, starting with the smallest matches and building up.
-        let mut dag = DiGraph::<String, ()>::new();
+        let mut dag = DiGraph::<BitSet, BitSet>::new();
         for (h1, h2) in &duplicates {
             // If h1 is not already in the bag (as it could be if it is used in
             // multiple matches), construct it by joining pieces in the bag.
@@ -83,25 +87,38 @@ impl Pathway {
         Self { dag }
     }
 
-    /// Get the pathway's directed, acyclic graph representation.
+    /// Get the pathway's directed, acyclic (multi)graph representation whose
+    /// nodes represent fragments and edges represent joining operations.
     ///
-    /// Nodes are fragments (represented as DOT strings) and edges `(u1, v)`
-    /// and `(u2, v)` exist if fragments `u1` and `u2` are joined to produce
-    /// fragment `v`. If `u` is duplicated and joined to itself to produce `v`,
-    /// then the edge `(u, v)` is included twice.
-    pub fn dag(&self) -> &DiGraph<String, ()> {
+    /// In detail, each node is labeled by a BitSet indexing the bonds its
+    /// fragment contains. An edge `(u1, v)` labeled `l1` and an edge `(u2, v)`
+    /// labeled `l2` exist iff the fragment BitSet `l1` is isomorphic to `u1`,
+    /// the fragment BitSet `l2` is isomorphic to `u2`, and `l1` and `l2` are
+    /// joined to produce a fragment isomorphic to `v`.
+    ///
+    /// Note that an edge `(u, v)` may be in the DAG multiple times, e.g., if
+    /// two fragments isomorphic to `u` are joined to produce a fragment
+    /// isomorphic to `v`. It may even be in the DAG multiple times *with the
+    /// same label*, e.g., if some fragment isomorphic to `u` is duplicated and
+    /// joined to itself to produce a fragment isomorphic to `v`.
+    pub fn dag(&self) -> &DiGraph<BitSet, BitSet> {
         &self.dag
     }
 }
 
 impl fmt::Display for Pathway {
-    /// Format the assembly pathway DAG as a DOT string whose node labels are
-    /// the DOT strings of their respective fragments.
+    /// Format the assembly pathway DAG as a DOT string whose node and edge
+    /// labels are the BitSets of their respective fragments' bonds.
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(
             f,
             "{:?}",
-            Dot::with_config(self.dag(), &[Config::EdgeNoLabel])
+            Dot::with_attr_getters(
+                self.dag(),
+                &[Config::EdgeNoLabel, Config::NodeNoLabel], // Suppress default labels.
+                &|_, e| format!("label = \"{}\" ", e.weight()),
+                &|_, (_, n_weight)| format!("label = \"{}\" ", n_weight)
+            )
         )
     }
 }
@@ -109,7 +126,7 @@ impl fmt::Display for Pathway {
 /// Helper function for [`Pathway::new`] that takes as input an edge-disjoint
 /// set of pieces and repeatedly joins all compatible pairs of pieces together,
 /// tracking these join operations in the given assembly pathway.
-fn join_pieces(mol: &Molecule, pieces: &mut Vec<BitSet>, pathway: &mut DiGraph<String, ()>) {
+fn join_pieces(mol: &Molecule, pieces: &mut Vec<BitSet>, pathway: &mut DiGraph<BitSet, BitSet>) {
     // For each piece, identify the set of nodes it contains.
     let mut piece_nodes = Vec::<BitSet>::new();
     for piece in pieces.iter() {
@@ -130,44 +147,22 @@ fn join_pieces(mol: &Molecule, pieces: &mut Vec<BitSet>, pathway: &mut DiGraph<S
         while j < pieces.len() {
             // If pieces i and j share a node, join them into one piece.
             if !piece_nodes[i].is_disjoint(&piece_nodes[j]) {
-                // Get the DOT string representations of pieces i and j.
-                let dot_i = format!(
-                    "{:?}",
-                    Dot::new(&subgraph_from_edge_mask(mol.graph(), &pieces[i]))
-                );
-                let dot_j = format!(
-                    "{:?}",
-                    Dot::new(&subgraph_from_edge_mask(mol.graph(), &pieces[j]))
-                );
-
-                // Ensure pieces i and j exist as nodes in the pathway DAG.
-                let i_ix = match pathway.raw_nodes().iter().position(|v| v.weight == dot_i) {
-                    Some(v_ix) => NodeIndex::new(v_ix),
-                    _ => pathway.add_node(dot_i),
-                };
-                let j_ix = match pathway.raw_nodes().iter().position(|v| v.weight == dot_j) {
-                    Some(v_ix) => NodeIndex::new(v_ix),
-                    _ => pathway.add_node(dot_j),
-                };
-
                 // Join piece j into piece i and update the nodes contained in
                 // the newly formed piece.
+                let piece_i = pieces[i].clone();
                 let piece_j = pieces.swap_remove(j);
                 pieces[i].union_with(&piece_j);
                 let piece_nodes_j = piece_nodes.swap_remove(j);
                 piece_nodes[i].union_with(&piece_nodes_j);
 
-                // Get the DOT string representation of the new piece and add
-                // it as a node to the pathway DAG.
-                let dot_joined = format!(
-                    "{:?}",
-                    Dot::new(&subgraph_from_edge_mask(mol.graph(), &pieces[i]))
-                );
-                let joined_ix = pathway.add_node(dot_joined);
+                // Ensure pieces i and j exist as nodes in the pathway DAG.
+                let i_ix = find_or_add_dag_node(mol, &piece_i, pathway);
+                let j_ix = find_or_add_dag_node(mol, &piece_j, pathway);
 
-                // Add edges in the pathway DAG representing the join.
-                pathway.add_edge(i_ix, joined_ix, ());
-                pathway.add_edge(j_ix, joined_ix, ());
+                // Add the joined piece and its incoming edges to the DAG.
+                let joined_ix = pathway.add_node(pieces[i].clone());
+                pathway.add_edge(i_ix, joined_ix, piece_i);
+                pathway.add_edge(j_ix, joined_ix, piece_j);
 
                 joined_piece_i = true;
             } else {
@@ -182,5 +177,23 @@ fn join_pieces(mol: &Molecule, pieces: &mut Vec<BitSet>, pathway: &mut DiGraph<S
         if !joined_piece_i {
             i += 1
         }
+    }
+}
+
+// Helper function for [`join_pieces`] that finds the pathway node isomorphic
+// to the given piece or adds a new node if one doesn't exist.
+fn find_or_add_dag_node(
+    mol: &Molecule,
+    piece: &BitSet,
+    pathway: &mut DiGraph<BitSet, BitSet>,
+) -> NodeIndex {
+    let piece_label = canonize(mol, piece, CanonizeMode::TreeNauty);
+    match pathway
+        .raw_nodes()
+        .iter()
+        .position(|v| canonize(mol, &v.weight, CanonizeMode::TreeNauty) == piece_label)
+    {
+        Some(v_ix) => NodeIndex::new(v_ix),
+        _ => pathway.add_node(piece.clone()),
     }
 }
